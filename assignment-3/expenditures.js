eaa2bfef048bbbc03aecbe32c7c04e1ed399b37a
tfleischauer <tfleischauer@gmail.com>
2014-01-28 17:55:42 -0800

    Web 150: Assignment 3
    Around the World
    
    Extracting and manipulating data in Objects with Loops.

---
+++ assignment-3/expenditures.js
@@ -0,0 +1,534 @@
+var expenditures = [
+  {
+    "amount": "3.244116656",
+    "country": "Zimbabwe"
+  },
+  {
+    "amount": "1.54930205",
+    "country": "Zambia"
+  },
+  {
+    "amount": "4.020736877",
+    "country": "Yemen, Rep."
+  },
+  {
+    "amount": "2.156886368",
+    "country": "Vietnam"
+  },
+  {
+    "amount": "1.051697628",
+    "country": "Venezuela, RB"
+  },
+  {
+    "amount": "1.946191062",
+    "country": "Uruguay"
+  },
+  {
+    "amount": "4.201260727",
+    "country": "United States"
+  },
+  {
+    "amount": "2.453601118",
+    "country": "United Kingdom"
+  },
+  {
+    "amount": "2.766435113",
+    "country": "Ukraine"
+  },
+  {
+    "amount": "1.44595004",
+    "country": "Uganda"
+  },
+  {
+    "amount": "2.30509689",
+    "country": "Turkey"
+  },
+  {
+    "amount": "1.553579602",
+    "country": "Tunisia"
+  },
+  {
+    "amount": "2.915699923",
+    "country": "Timor-Leste"
+  },
+  {
+    "amount": "1.471990002",
+    "country": "Thailand"
+  },
+  {
+    "amount": "1.131792601",
+    "country": "Tanzania"
+  },
+  {
+    "amount": "0.765395409",
+    "country": "Switzerland"
+  },
+  {
+    "amount": "1.185533181",
+    "country": "Sweden"
+  },
+  {
+    "amount": "3.174809707",
+    "country": "Swaziland"
+  },
+  {
+    "amount": "2.426679975",
+    "country": "Sri Lanka"
+  },
+  {
+    "amount": "0.872107148",
+    "country": "Spain"
+  },
+  {
+    "amount": "9.43291162",
+    "country": "South Sudan"
+  },
+  {
+    "amount": "2.456006582",
+    "country": "South Asia"
+  },
+  {
+    "amount": "1.163382929",
+    "country": "South Africa"
+  },
+  {
+    "amount": "1.175038224",
+    "country": "Slovenia"
+  },
+  {
+    "amount": "1.122426016",
+    "country": "Slovak Republic"
+  },
+  {
+    "amount": "3.516895015",
+    "country": "Singapore"
+  },
+  {
+    "amount": "0.720329281",
+    "country": "Sierra Leone"
+  },
+  {
+    "amount": "0.814559871",
+    "country": "Seychelles"
+  },
+  {
+    "amount": "2.205520028",
+    "country": "Serbia"
+  },
+  {
+    "amount": "7.977502554",
+    "country": "Saudi Arabia"
+  },
+  {
+    "amount": "1.122993213",
+    "country": "Rwanda"
+  },
+  {
+    "amount": "4.471313338",
+    "country": "Russian Federation"
+  },
+  {
+    "amount": "1.289703445",
+    "country": "Romania"
+  },
+  {
+    "amount": "1.780666684",
+    "country": "Portugal"
+  },
+  {
+    "amount": "1.909966119",
+    "country": "Poland"
+  },
+  {
+    "amount": "1.190187897",
+    "country": "Philippines"
+  },
+  {
+    "amount": "1.253145169",
+    "country": "Peru"
+  },
+  {
+    "amount": "1.662685018",
+    "country": "Paraguay"
+  },
+  {
+    "amount": "0.536525634",
+    "country": "Papua New Guinea"
+  },
+  {
+    "amount": "3.125799182",
+    "country": "Pakistan"
+  },
+  {
+    "amount": "8.607026797",
+    "country": "Oman"
+  },
+  {
+    "amount": "1.395823744",
+    "country": "Norway"
+  },
+  {
+    "amount": "0.886399596",
+    "country": "Nigeria"
+  },
+  {
+    "amount": "1.02952808",
+    "country": "Niger"
+  },
+  {
+    "amount": "0.626058497",
+    "country": "Nicaragua"
+  },
+  {
+    "amount": "1.129656261",
+    "country": "New Zealand"
+  },
+  {
+    "amount": "1.277242558",
+    "country": "Netherlands"
+  },
+  {
+    "amount": "3.113964388",
+    "country": "Namibia"
+  },
+  {
+    "amount": "3.545170128",
+    "country": "Morocco"
+  },
+  {
+    "amount": "1.892370511",
+    "country": "Montenegro"
+  },
+  {
+    "amount": "1.115148773",
+    "country": "Mongolia"
+  },
+  {
+    "amount": "0.300522499",
+    "country": "Moldova"
+  },
+  {
+    "amount": "0.59273148",
+    "country": "Mexico"
+  },
+  {
+    "amount": "0.193647997",
+    "country": "Mauritius"
+  },
+  {
+    "amount": "0.607517161",
+    "country": "Malta"
+  },
+  {
+    "amount": "1.444156209",
+    "country": "Mali"
+  },
+  {
+    "amount": "1.54137587",
+    "country": "Malaysia"
+  },
+  {
+    "amount": "0.930577015",
+    "country": "Malawi"
+  },
+  {
+    "amount": "0.689653075",
+    "country": "Madagascar"
+  },
+  {
+    "amount": "1.378498729",
+    "country": "Macedonia, FYR"
+  },
+  {
+    "amount": "0.617459422",
+    "country": "Luxembourg"
+  },
+  {
+    "amount": "0.966283817",
+    "country": "Lithuania"
+  },
+  {
+    "amount": "0.833191831",
+    "country": "Liberia"
+  },
+  {
+    "amount": "1.94082958",
+    "country": "Lesotho"
+  },
+  {
+    "amount": "4.040778499",
+    "country": "Lebanon"
+  },
+  {
+    "amount": "0.921403626",
+    "country": "Latvia"
+  },
+  {
+    "amount": "2.802839697",
+    "country": "Korea, Rep."
+  },
+  {
+    "amount": "1.959643791",
+    "country": "Kenya"
+  },
+  {
+    "amount": "1.196166013",
+    "country": "Kazakhstan"
+  },
+  {
+    "amount": "4.680073485",
+    "country": "Jordan"
+  },
+  {
+    "amount": "0.994471629",
+    "country": "Japan"
+  },
+  {
+    "amount": "0.863408304",
+    "country": "Jamaica"
+  },
+  {
+    "amount": "1.688247045",
+    "country": "Italy"
+  },
+  {
+    "amount": "5.688140814",
+    "country": "Israel"
+  },
+  {
+    "amount": "0.550208005",
+    "country": "Ireland"
+  },
+  {
+    "amount": "2.879849562",
+    "country": "Iraq"
+  },
+  {
+    "amount": "0.78182554",
+    "country": "Indonesia"
+  },
+  {
+    "amount": "2.432983189",
+    "country": "India"
+  },
+  {
+    "amount": "0.128466881",
+    "country": "Iceland"
+  },
+  {
+    "amount": "0.833422823",
+    "country": "Hungary"
+  },
+  {
+    "amount": "1.051805277",
+    "country": "Honduras"
+  },
+  {
+    "amount": "1.085372698",
+    "country": "Guyana"
+  },
+  {
+    "amount": "2.016353777",
+    "country": "Guinea-Bissau"
+  },
+  {
+    "amount": "0.4205721",
+    "country": "Guatemala"
+  },
+  {
+    "amount": "2.625562526",
+    "country": "Greece"
+  },
+  {
+    "amount": "0.268092481",
+    "country": "Ghana"
+  },
+  {
+    "amount": "1.335921092",
+    "country": "Germany"
+  },
+  {
+    "amount": "2.884586908",
+    "country": "Georgia"
+  },
+  {
+    "amount": "1.364314645",
+    "country": "Gabon"
+  },
+  {
+    "amount": "2.256461742",
+    "country": "France"
+  },
+  {
+    "amount": "1.479684846",
+    "country": "Finland"
+  },
+  {
+    "amount": "1.45836214",
+    "country": "Fiji"
+  },
+  {
+    "amount": "0.91635635",
+    "country": "Ethiopia"
+  },
+  {
+    "amount": "1.877677553",
+    "country": "Estonia"
+  },
+  {
+    "amount": "0.993111078",
+    "country": "El Salvador"
+  },
+  {
+    "amount": "1.682020181",
+    "country": "Egypt, Arab Rep."
+  },
+  {
+    "amount": "2.83079971",
+    "country": "Ecuador"
+  },
+  {
+    "amount": "0.614558882",
+    "country": "Dominican Republic"
+  },
+  {
+    "amount": "1.410647566",
+    "country": "Denmark"
+  },
+  {
+    "amount": "1.130390964",
+    "country": "Czech Republic"
+  },
+  {
+    "amount": "2.071082719",
+    "country": "Cyprus"
+  },
+  {
+    "amount": "1.699411493",
+    "country": "Croatia"
+  },
+  {
+    "amount": "1.650793651",
+    "country": "Cote d'Ivoire"
+  },
+  {
+    "amount": "1.788484322",
+    "country": "Congo, Dem. Rep."
+  },
+  {
+    "amount": "3.283805362",
+    "country": "Colombia"
+  },
+  {
+    "amount": "2.019940996",
+    "country": "China"
+  },
+  {
+    "amount": "2.044023219",
+    "country": "Chile"
+  },
+  {
+    "amount": "1.237604858",
+    "country": "Canada"
+  },
+  {
+    "amount": "1.4001313",
+    "country": "Cameroon"
+  },
+  {
+    "amount": "1.547243915",
+    "country": "Cambodia"
+  },
+  {
+    "amount": "2.386140261",
+    "country": "Burundi"
+  },
+  {
+    "amount": "1.393905092",
+    "country": "Burkina Faso"
+  },
+  {
+    "amount": "1.459099006",
+    "country": "Bulgaria"
+  },
+  {
+    "amount": "2.426233532",
+    "country": "Brunei Darussalam"
+  },
+  {
+    "amount": "1.47176503",
+    "country": "Brazil"
+  },
+  {
+    "amount": "2.299326598",
+    "country": "Botswana"
+  },
+  {
+    "amount": "1.368207687",
+    "country": "Bosnia and Herzegovina"
+  },
+  {
+    "amount": "1.465639812",
+    "country": "Bolivia"
+  },
+  {
+    "amount": "1.034161008",
+    "country": "Benin"
+  },
+  {
+    "amount": "0.946532021",
+    "country": "Belize"
+  },
+  {
+    "amount": "1.052726794",
+    "country": "Belgium"
+  },
+  {
+    "amount": "1.204812424",
+    "country": "Belarus"
+  },
+  {
+    "amount": "1.346252331",
+    "country": "Bangladesh"
+  },
+  {
+    "amount": "3.135922075",
+    "country": "Bahrain"
+  },
+  {
+    "amount": "4.637466432",
+    "country": "Azerbaijan"
+  },
+  {
+    "amount": "0.818556707",
+    "country": "Austria"
+  },
+  {
+    "amount": "1.713584714",
+    "country": "Australia"
+  },
+  {
+    "amount": "3.902311339",
+    "country": "Armenia"
+  },
+  {
+    "amount": "0.910062956",
+    "country": "Argentina"
+  },
+  {
+    "amount": "3.633899794",
+    "country": "Angola"
+  },
+  {
+    "amount": "4.563523067",
+    "country": "Algeria"
+  },
+  {
+    "amount": "1.45505862",
+    "country": "Albania"
+  },
+  {
+    "amount": "3.552340322",
+    "country": "Afghanistan"
+  }
+]
